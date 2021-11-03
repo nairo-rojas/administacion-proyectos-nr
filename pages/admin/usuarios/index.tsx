1539c6bffe17b023c93c7a245a74f13e00c03ca3
Nairo Rojas <nairorojas75@gmail.com>
2021-11-03 15:36:36 -0500

    Agregar Link en el Index de usuarios

--- pages/admin/usuarios/index.tsx
+++ pages/admin/usuarios/index.tsx
@@ -1,6 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
 const usuarios = ()=>{
     return(
-        <div className='bg-blue-300 flex justify-center'>Soy la pagina de usuarios, eso es todo, tal cual</div>
+        <div className='bg-blue-300 flex justify-center'>
+            <h1>Soy la pagina de usuarios, eso es todo, tal cual</h1>
+            <Link href="/">
+                <a>
+                    Ir a Home
+                </a>
+            </Link>
+            
+            
+        </div>
+
 
     )
 };
